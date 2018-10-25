79a807b4b5a61e0588fe429660f2f107ef472987
prateek agrawal <prateekagrawal89760@gmail.com>
2018-10-26 00:34:22 +0530

    Initial commit

---
+++ set1/program1.py
@@ -0,0 +1,10 @@
+ans = set()
+come = set()
+n = int(input())
+arr = [int(x) for x in input().split()]
+for i in arr:
+    if i in come:
+        ans.add(i)
+    else:
+        come.add(i)
+print(*ans)
