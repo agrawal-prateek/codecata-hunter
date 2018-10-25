9e3f65040f148cb9a641f4698c791b2d1252e34b
prateek agrawal <prateekagrawal89760@gmail.com>
2018-10-26 00:39:42 +0530

    Commited from Pycharm

--- set1/program1.py
+++ set1/program1.py
@@ -1,10 +1,13 @@
-ans = set()
-come = set()
-n = int(input())
-arr = [int(x) for x in input().split()]
-for i in arr:
-    if i in come:
-        ans.add(i)
-    else:
-        come.add(i)
-print(*ans)
+try:
+    ans = set()
+    come = set()
+    n = int(input())
+    arr = [int(x) for x in input().split()]
+    for i in arr:
+        if i in come:
+            ans.add(i)
+        else:
+            come.add(i)
+    print(*ans)
+except Exception as e:
+    print('Invalid Input')
