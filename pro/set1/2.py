fd14ef929a828cae8f3ca89010883cac889e489a
prateek agrawal <prateekagrawal89760@gmail.com>
2018-10-26 02:47:52 +0530

    Commited from Pycharm

--- pro/set1/2.py
+++ pro/set1/2.py
@@ -1,2 +1,20 @@
-n, k = [x for x in input().split()]
-print(n[int(k):])
+def lowest(s, k, ans):
+    if not k:
+        ans.append(s)
+    elif len(s) <= k:
+        pass
+    else:
+        m = min(s[:k + 1])
+        i = s[:k + 1].index(m)
+        ans.append(m)
+        lowest(s[i + 1:], k - i, ans)
+
+
+try:
+    s, k = [x for x in input().split()]
+    k = int(k)
+    ans = []
+    lowest(s, k, ans)
+    print(*ans, sep='')
+except Exception as e:
+    print('Invalid Input')
