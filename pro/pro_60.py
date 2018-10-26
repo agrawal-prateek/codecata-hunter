5eba713fea7d72262864bfdfcf04192fa02f9d6e
prateek agrawal <prateekagrawal89760@gmail.com>
2018-10-26 23:44:49 +0530

    Commited from Pycharm

--- pro/pro_60.py
+++ pro/pro_60.py
@@ -1,22 +1,24 @@
 def pro_60():
-	n=int(input())
-	c=3
-	co=1
-	i=1
-	r=c
-	out=0
-	while(True):
-		c-=1
-		i+=1
-		if i>=n:
-			co+=1
-			out=1
-		if c==1:
-			c=2*r
-			r=c
-			i+=1
-			if out==1:
-				break
+    n = int(input())
+    c = 3
+    co = 1
+    i = 1
+    r = c
+    out = 0
+    while (True):
+        c -= 1
+        i += 1
+        if i >= n:
+            co += 1
+            out = 1
+        if c == 1:
+            c = 2 * r
+            r = c
+            i += 1
+            if out == 1:
+                break
 
-	print(co)
+    print(co)
+
+
 pro_60()
