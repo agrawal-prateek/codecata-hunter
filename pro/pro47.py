160d99c578615cf065399c9b4f8bfc584c417937
prateek agrawal <prateekagrawal89760@gmail.com>
2018-10-26 23:31:46 +0530

    Commited from Pycharm

--- pro/pro47.py
+++ pro/pro47.py
@@ -1,20 +1,22 @@
 def rounding():
-	n=int(input())
-	k=int(input())
-	bas=10
-	c=0
-	while(True):
-		tn=bas*n
-		t=tn
-		while( t%10==0):
-			c+=1
-			t//=10
-		if c==4:
-			print(tn)
-			break
-		c=0
-		bas+=10
+    n = int(input())
+    k = int(input())
+    bas = 10
+    c = 0
+    while (True):
+        tn = bas * n
+        t = tn
+        while (t % 10 == 0):
+            c += 1
+            t //= 10
+        if c == 4:
+            print(tn)
+            break
+        c = 0
+        bas += 10
+
+
 try:
-	rounding()
+    rounding()
 except:
-	print('invalid')
+    print('invalid')
--- pro/pro49.py
+++ pro/pro49.py
@@ -1,19 +1,23 @@
 def convert(a):
-	s=0
-	for i in range(len(a)):
-		s+=ord(a[i])
-	return(s)
+    s = 0
+    for i in range(len(a)):
+        s += ord(a[i])
+    return (s)
+
+
 def main():
-	str2=input()
-	str1=input()
-	x=convert(str1)
-	y=convert(str2)
-	d=x/y
-	if d==x//y:
-		print(int(d))
-	else :
-		print(-1)
+    str2 = input()
+    str1 = input()
+    x = convert(str1)
+    y = convert(str2)
+    d = x / y
+    if d == x // y:
+        print(int(d))
+    else:
+        print(-1)
+
+
 try:
-	main()
+    main()
 except:
-	print('invalid')
+    print('invalid')
