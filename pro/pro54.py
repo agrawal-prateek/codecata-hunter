058c426372ec70e804532924e0101e78a2910185
Prateek Agrawal <prateekagrawal89760@gmail.com>
2018-11-29 01:18:41 +0530

    Added Sol

---
+++ pro/pro54.py
@@ -0,0 +1,19 @@
+def maxmain2wd():
+    s = 0
+    l = []
+    max = -1
+    n = int(input())
+    for i in range(n):
+        l.append(int(input()))
+    for i in range(n):
+        if l[i] < l[i + 1]:
+            s += 1
+        else:
+            continue
+        if max < s:
+            max = s
+    print(max)
+
+
+
+
---
+++ pro/pro56.py
@@ -0,0 +1,15 @@
+def jwijeiuw():
+    s = 0
+    l = []
+    max = -1
+    n = int(input())
+    for i in range(n):
+        l.append(int(input()))
+    for i in range(n):
+        if l[i] < l[i + 1]:
+            s += 1
+        else:
+            continue
+        if max < s:
+            max = s
+    print(max)
